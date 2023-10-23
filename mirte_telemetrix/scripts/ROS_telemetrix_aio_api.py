46da90cf3c4b96cd6f3d739ea8dbae82e82b92b5
Arend-Jan van Hilten (TU) <a.j.vanhilten@tudelft.nl>
2023-10-23 13:45:21 +0200

    Add servo min-max pulse option

--- mirte_telemetrix/scripts/ROS_telemetrix_aio_api.py
+++ mirte_telemetrix/scripts/ROS_telemetrix_aio_api.py
@@ -383,12 +383,20 @@ class Servo:
         self.board = board
         self.pins = get_pin_numbers(servo_obj)
         self.name = servo_obj["name"]
-
+        if "min_pulse" in servo_obj:
+            self.min_pulse = servo_obj["min_pulse"]
+        else:
+            self.min_pulse = 544
+        if "max_pulse" in servo_obj:
+            self.max_pulse = servo_obj["max_pulse"]
+        else:
+            self.max_pulse = 544
+        
     async def stop(self):
         await board.detach_servo(self.pins["pin"])
 
     async def start(self):
-        await board.set_pin_mode_servo(self.pins["pin"])
+        await board.set_pin_mode_servo(self.pins["pin"], self.min_pulse, self.max_pulse)
         server = rospy.Service(
             "/mirte/set_" + self.name + "_servo_angle",
             SetServoAngle,
