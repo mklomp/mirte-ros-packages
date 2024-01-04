c34d35198614c043536a170a11af7d5eed0ddaa9
Arend-Jan van Hilten (TU) <a.j.vanhilten@tudelft.nl>
2024-01-05 00:39:41 +0100

    Fix styling + clang compiler issue

--- include/mirte-ping.hpp
+++ include/mirte-ping.hpp
@@ -11,7 +11,7 @@ public:
   ros::NodeHandle *nh;
   void ping_task();
   std::thread ping_thread; // TODO: jthread from c++20
-  void ping_callback(std::vector<uint8_t> pins);
+  void ping_callback(const std::vector<uint8_t> pins);
   uint8_t last_ping = 0;
   uint8_t magic = 0;
   bool first_magic = true;
--- src/mirte-ping.cpp
+++ src/mirte-ping.cpp
@@ -1,56 +1,46 @@
 #include "mirte-ping.hpp"
 
 Mirte_Ping::Mirte_Ping(TMX &tmx, ros::NodeHandle &nh,
-                       std::function<void()> stop_func)
-{
+                       std::function<void()> stop_func) {
   this->tmx = &tmx;
   this->nh = &nh;
   this->ping_thread = std::thread(&Mirte_Ping::ping_task, this);
   this->tmx->add_callback(
       TMX::MESSAGE_IN_TYPE::PONG_REPORT,
       std::bind(&Mirte_Ping::ping_callback, this, std::placeholders::_1));
+  this->tmx->add_callback(TMX::MESSAGE_IN_TYPE::ANALOG_REPORT,
+                          [](std::vector<uint8_t> t) { t[1] = 3; });
   this->stop_func = stop_func;
 }
 
-void Mirte_Ping::ping_task()
-{
+void Mirte_Ping::ping_task() {
   uint8_t num = 0;
   std::this_thread::sleep_for(std::chrono::milliseconds(1000));
-  while (!this->is_stopped)
-  {
+  while (!this->is_stopped) {
     num++;
     this->tmx->sendPing(num);
     std::this_thread::sleep_for(std::chrono::milliseconds(400));
-    if ((num - this->last_ping) > 2)
-    {
+    if ((num - this->last_ping) > 2) {
       std::cout << "stop!!!" << (int)((num - this->last_ping)) << std::endl;
       this->stop_func();
     }
   }
 }
 
-void Mirte_Ping::ping_callback(std::vector<uint8_t> message)
-{
-  if (message.size() != 3)
-  {
-    std::cout << "ping message size != 3" << message.size() << std::endl;
-    return;
-  }
-  if (this->first_magic)
-  {
-    this->magic = message[1];
+void Mirte_Ping::ping_callback(const std::vector<uint8_t> message) {
+  if (this->first_magic) {
+    this->magic = message[3];
     this->first_magic = false;
   }
-  if (this->magic != message[1])
-  {
-    std::cout << "magic changed" << (int)this->magic << "-> " << (int)message[1] << std::endl;
+  if (this->magic != message[3]) {
+    std::cout << "magic changed" << (int)this->magic << "-> " << (int)message[3]
+              << std::endl;
     this->stop_func();
   }
   this->last_ping = message[2];
 }
 
-void Mirte_Ping::stop()
-{
+void Mirte_Ping::stop() {
   this->is_stopped = true;
   this->ping_thread.join();
   this->stop_func = []() {};
