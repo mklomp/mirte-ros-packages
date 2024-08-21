90404e85e4d9b4a94fd80462cab55d63d9c81a5f
Arend-Jan van Hilten <arendjan18@gmail.com>
2024-08-21 16:19:43 +0200

    upd launch file mirte master

--- mirte_bringup/launch/minimal_master.launch.py
+++ mirte_bringup/launch/minimal_master.launch.py
@@ -1,8 +1,11 @@
 from launch import LaunchDescription
 from launch.actions import IncludeLaunchDescription
 from launch.launch_description_sources import PythonLaunchDescriptionSource
+from launch_xml.launch_description_sources import XMLLaunchDescriptionSource
+
 from launch.substitutions import PathJoinSubstitution
 from launch_ros.substitutions import FindPackageShare
+from launch_ros.actions import Node
 
 def generate_launch_description():
     ld = LaunchDescription()
@@ -17,17 +20,6 @@ def generate_launch_description():
         ])
     )
 
-
-#     diff_drive_control=IncludeLaunchDescription(
-#         PythonLaunchDescriptionSource([
-#             PathJoinSubstitution([
-#                 FindPackageShare('mirte_control'),
-#                 'launch',
-#                 'diffbot.launch.py'
-#             ])
-#         ])
-#     )
-
     mecanum_drive_control=IncludeLaunchDescription(
         PythonLaunchDescriptionSource([
             PathJoinSubstitution([
@@ -37,7 +29,35 @@ def generate_launch_description():
             ])
         ])
     )
+    
+    web_video_server=Node(
+        package = 'web_video_server',
+        executable = 'web_video_server', 
+        output='screen'
+    )
+
+    depth_cam = IncludeLaunchDescription(
+            XMLLaunchDescriptionSource([
+            PathJoinSubstitution([
+                FindPackageShare('astra_camera'),
+                'launch',
+                'astro_pro_plus.launch.xml'
+            ])
+        ])
+    )
+    lidar = IncludeLaunchDescription(
+        PythonLaunchDescriptionSource([
+            PathJoinSubstitution([
+                FindPackageShare('rplidar_ros'),
+                'launch',
+                'rplidar_c1_launch.py'
+            ])
+        ])
+    )
 
     ld.add_action(telemetrix)
     ld.add_action(mecanum_drive_control)
+    ld.add_action(web_video_server)
+    ld.add_action(lidar)
+    ld.add_action(depth_cam)
     return ld
