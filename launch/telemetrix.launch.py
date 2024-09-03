94519b7852362845cded65b3986ca64678985f0b
SuperJappie08 <36795178+SuperJappie08@users.noreply.github.com>
2024-09-03 15:34:07 +0200

    Convert pcb configs

--- launch/telemetrix.launch.py
+++ launch/telemetrix.launch.py
@@ -1,53 +1,37 @@
-import os
 from ament_index_python.packages import get_package_share_directory
+
 from launch import LaunchDescription
+from launch.actions import DeclareLaunchArgument
+from launch.substitutions import LaunchConfiguration, PathJoinSubstitution
 from launch_ros.actions import Node
-from launch_ros.actions import ComposableNodeContainer
-import yaml
-from launch_ros.descriptions import ComposableNode
+
 
 def generate_launch_description():
-    ld = LaunchDescription()
+    param_config_arg = DeclareLaunchArgument(
+        "config_path",
+        default_value=PathJoinSubstitution((
+            get_package_share_directory("mirte_telemetrix_cpp"),
+            "config",
+            "mirte_user_config.yaml",
+        )),
+    )
+    ld = LaunchDescription([param_config_arg])
 
-    config = os.path.join(
-        get_package_share_directory('mirte_telemetrix_cpp'),
-        'config',
-        'mirte_user_config.yaml'
-        )
-    param_config = os.path.join(
-        get_package_share_directory('mirte_telemetrix_cpp'),
-        'config',
-        'mirte_user_config.yaml'
-        )
-    
-    with open(param_config, 'r') as f:
-        params = yaml.safe_load(f)
-    print(params)
-    # container = ComposableNodeContainer(
-    #         package = 'mirte_telemetrix_cpp',
-    #         name = 'mirte_telemetrix',
-    #         executable = 'mirte_telemetrix_cpp_node',
-    #         node_namespace='/mirte',
-    #         composable_node_descriptions=[
-    #             ComposableNode(
-    #                 node_name='ros2_to_serial_bridge',
-    #                 node_namespace='/mirte',
-    #                 package='ros2_serial_example',
-    #                 node_plugin='ros2_to_serial_bridge::ROS2ToSerialBridge',
-    #                 parameters=[params]),
-    #         ],
-    #         output='screen',
+    # config = os.path.join(
+    # get_package_share_directory("mirte_telemetrix_cpp"),
+    # "config",
+    #     "mirte_user_config.yaml",
     # )
 
-    node=Node(
-        package = 'mirte_telemetrix_cpp',
-        name = 'mirte_telemetrix',
-        executable = 'mirte_telemetrix_cpp_node',
-        parameters = [config],
-        output='screen',
-        namespace='/mirte',
+    node = Node(
+        package="mirte_telemetrix_cpp",
+        name="mirte_telemetrix",
+        executable="mirte_telemetrix_cpp_node",
+        parameters=[LaunchConfiguration("config_path")],
+        output="screen",
+        namespace="/mirte",
         # node_namespace='/mirte',
-        emulate_tty=True
+        emulate_tty=True,
     )
 
     ld.add_action(node)
