6ffa7f0528a4b3ae1e74977131b27fd23a46b672
SuperJappie08 <36795178+SuperJappie08@users.noreply.github.com>
2024-09-12 12:08:06 +0200

    Remove Python version, Add linting

--- launch/telemetrix.launch.py
+++ launch/telemetrix.launch.py
@@ -1,33 +1,43 @@
-from ament_index_python.packages import get_package_share_directory
-
 from launch import LaunchDescription
 from launch.actions import DeclareLaunchArgument
 from launch.substitutions import LaunchConfiguration, PathJoinSubstitution
 from launch_ros.actions import Node
+from launch_ros.substitutions import FindPackageShare
 
 
 def generate_launch_description():
-    param_config_arg = DeclareLaunchArgument(
-        "config_path",
-        default_value=PathJoinSubstitution((
-            get_package_share_directory("mirte_telemetrix_cpp"),
-            "config",
-            "mirte_user_config.yaml",
-        )),
-    )
+    launch_arguments: list[DeclareLaunchArgument] = [
+        DeclareLaunchArgument(
+            "config_path",
+            default_value=PathJoinSubstitution(
+                (
+                    FindPackageShare("mirte_telemetrix_cpp"),
+                    "config",
+                    "mirte_user_config.yaml",
+                )
+            ),
+        ),
+        DeclareLaunchArgument(
+            "hardware_namespace",
+            default_value="io",
+            description="The namespace for the Telemetrix Node and the hardware peripherals",
+        ),
+    ]
 
-    ld = LaunchDescription([param_config_arg])
+    ld = LaunchDescription(launch_arguments)
 
     node = Node(
         package="mirte_telemetrix_cpp",
-        name="mirte_telemetrix",
+        name="telemetrix",
         executable="mirte_telemetrix_cpp_node",
         parameters=[LaunchConfiguration("config_path")],
         output="screen",
         emulate_tty=True,
+        namespace=LaunchConfiguration("hardware_namespace"),
         respawn=True,
         respawn_delay=5,
-        # respawn_max_retries=10  # TODO: Not avialable yet in humble (avialable starting from jazzy)
+        # TODO: Not avialable yet in humble (avialable starting from jazzy)
+        # respawn_max_retries=10
     )
 
     ld.add_action(node)
